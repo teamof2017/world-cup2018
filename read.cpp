1571dcf2a64d0b20c403ca11fbd42933080dfa26
siavash <siavash_ghasemi77@yahoo.com>
2018-01-10 05:21:45 -0800

    this is updated

--- read.cpp
+++ read.cpp
@@ -21,8 +21,12 @@ typedef struct infoteams{
 	players *p;
 }teams;
 
+
+char groupArray[20][32];
+
 teams team_array[40];
 players player_array[40];
+
 
 
 
@@ -65,9 +69,9 @@ void ReadFromFileTeaminfo(void){
 		team_array[i].seed=seed;
 		strcpy(team_array[i].filename,filename);
 	}
-	for(int j=0;j<i;j++){
+/*	for(int j=0;j<i;j++){
 		printf("name=%s 	  group=%c 	 placeInGroup=%d 	 confedration=%s   seed=%d    filename=%s\n",team_array[j].name,team_array[j].group,team_array[j].placeInGroup,team_array[j].confedration,team_array[j].seed,team_array[j].filename);
-	}
+	}*/
 	fclose(fp);
 }
 void ReadFromFilePlayerinfo(void){
@@ -93,7 +97,53 @@ void ReadFromFilePlayerinfo(void){
 	fclose(fp);
 	}
 }
+
+void search_group(){
+	
+	int i=0 , j=0;
+	char y = 'A';
+	char *x=0;
+	int cnt =0 ;
+	 x = (char*)calloc(20 , sizeof(char));
+	for(cnt=0 ; cnt<32 ; cnt++){
+		for(i=0 , j=0 ; i<32 ; i++){
+			
+			
+		if(team_array[i].group == y){
+
+			strcpy(x , (team_array[i].name )) ;
+			j++;
+			printf("%s\n", x);
+
+			if(j % 4 == 0)
+				y++;
+				
+			if(y > 'G')
+			  return;
+
+		}
+	
+}
+
+		puts("");
+}
+}
+
+
+void print_groups(){
+	int i=0;
+	for(i=0 ; i<32 ;i++){
+		
+		printf("%s\n", groupArray[i]);
+	}	
+
+		
+
+	
+}
 int main(){
 	ReadFromFileTeaminfo();
 	//ReadFromFilePlayerinfo();
+	search_group();
+	//print_groups();
 }
