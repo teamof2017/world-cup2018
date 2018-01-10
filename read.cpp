149c3401381ea9b0e46ce5b6f3b365228480232a
FFlowersH <alikargar518@gmail.com>
2018-01-10 16:42:39 +0330

    updated read.cpp

--- read.cpp
+++ read.cpp
@@ -21,10 +21,7 @@ typedef struct infoteams{
 	players *p;
 }teams;
 
-teams team_array[40];
-players player_array[40];
-
-
+teams team_array[32];
 
 
 void RemoveChars(char *s, char c)
@@ -50,14 +47,18 @@ void ReadFromFileTeaminfo(void){
 	FILE *fp=fopen("Teams.csv","r");
 	char tmp[100];
 	char TeamName[20];
+	char TeamName2[20];
 	char group;
 	int placeInGroup,seed,i=0;
 	char confedration[20];
-	char filename[40];
+	char filename[20];
+	char filename2[20];
 	for( i=0;fgets(tmp,100,fp)!=NULL;i++){
 		RemoveChars(tmp,',');
-		sscanf(tmp,"%s %c %d %s %d %s", TeamName , &group  , &placeInGroup , confedration , &seed , filename );
-		//printf("name=%s 	  group=%c 	 placeInGroup=%d 	 confedration=%s   seed=%d    filename=%s\n",TeamName,group,placeInGroup,confedration,seed,filename);
+		sscanf(tmp,"%5s %s %c %d %s %d %5s %s", TeamName,TeamName2 , &group  , &placeInGroup , confedration , &seed , filename,filename2 );
+		printf("%d. 	name=%s 	  group=%c 	 placeInGroup=%d 	\n 	confedration=%s   seed=%d    filename=%s\n\n\n\n\n\n",i+1,TeamName,group,placeInGroup,confedration,seed,filename);
+		strcat(TeamName,TeamName2);
+		strcat(filename,filename2);
 		strcpy(team_array[i].name , TeamName);
 		team_array[i].group=group;
 		team_array[i].placeInGroup=placeInGroup;
@@ -65,19 +66,23 @@ void ReadFromFileTeaminfo(void){
 		team_array[i].seed=seed;
 		strcpy(team_array[i].filename,filename);
 	}
+	puts("\n\n\n\n\n now in array: \n\n\n\n\n");
 	for(int j=0;j<i;j++){
-		printf("name=%s 	  group=%c 	 placeInGroup=%d 	 confedration=%s   seed=%d    filename=%s\n",team_array[j].name,team_array[j].group,team_array[j].placeInGroup,team_array[j].confedration,team_array[j].seed,team_array[j].filename);
+		printf("%d. 	name=%s 	  group=%c 	 placeInGroup=%d 	 \n	 confedration=%s   seed=%d    filename=%s\n\n\n\n\n\n\n",j+1,team_array[j].name,team_array[j].group,team_array[j].placeInGroup,team_array[j].confedration,team_array[j].seed,team_array[j].filename);
 	}
+	printf("\n\n\n\n\n\n\"Teaminfo\"\n\n\n\n\n");
 	fclose(fp);
 }
 void ReadFromFilePlayerinfo(void){
-	for(int count=0;count<32;count++){
-	FILE *fp=fopen(team_array[count].filename,"r");
+	//for(int count=0;count<32;count++){
+	//FILE *fpo=fopen(team_array[count].filename,"r");
+	FILE *fpo=fopen("Russia.csv","r");
 	char tmp[100];//tmp is temp and first name in this function
 	char mainpost;
 	char lastname[20];
+	players player_array[40];
 	int num, i=0,age;
-	for( i=0;fgets(tmp,100,fp)!=NULL;i++){
+	for( i=0;fgets(tmp,100,fpo)!=NULL;i++){
 		RemoveChars(tmp,',');
 		sscanf(tmp,"%d %s %s %d %c",&num , tmp  , lastname , &age,&mainpost);
 		//printf("num=%d	name=%s %s   age=%d    mainpost=%c\n",num,tmp,lastname,age,mainpost);
@@ -87,13 +92,14 @@ void ReadFromFilePlayerinfo(void){
 		strcat(tmp,lastname);
 		strcpy(player_array[i].playername,tmp);
 	}
-	/*for(int j=0;j<i;j++){
+	for(int j=0;j<i;j++){
 		printf("num=%d 	  name=%s 	 age=%d 	 mainpost=%c\n",player_array[j].num,player_array[j].playername,player_array[j].age,player_array[j].mainpost);
-	}*/	
-	fclose(fp);
-	}
+	}	
+	fclose(fpo);
+//	printf("\n\n\n\n\n\n\"Team : %s\"\n\n\n\n\n",team_array[count].name);
+	//}
 }
 int main(){
 	ReadFromFileTeaminfo();
-	//ReadFromFilePlayerinfo();
+//	ReadFromFilePlayerinfo();
 }
